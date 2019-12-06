38b17487051427b213ea9b5ea89d8100e7b0271d
Germain Ol <djuhngleen@gmail.com>
2019-12-06 13:10:14 +0100

    ajout bd v2

--- Analyse/powerdesigner/script/projet_s3_v2.sql
+++ Analyse/powerdesigner/script/projet_s3_v2.sql
@@ -1,12 +1,11 @@
 /*==============================================================*/
 /* Nom de SGBD :  MySQL 5.0                                     */
-/* Date de création :  01/12/2019 23:16:53                      */
+/* Date de création :  06/12/2019 13:08:21                      */
 /*==============================================================*/
 
 
-drop trigger INSERT_PASS_TO_SHA;
-
-drop trigger UPDATE_PASS_TO_SHA;
+alter table ADRESSE 
+   drop foreign key FK_ADRESSE_AVOIR_ADR_ENTREPRI;
 
 alter table CANDIDATER 
    drop foreign key FK_CANDIDAT_CANDIDATE_ETUDIANT;
@@ -15,19 +14,16 @@ alter table CANDIDATER
    drop foreign key FK_CANDIDAT_CANDIDATE_CANDIDAT;
 
 alter table CANDIDATURE 
-   drop foreign key FK_CANDIDAT_POSSEDER_TYPE_CAN;
+   drop foreign key FK_CANDIDAT_LIER_PROJ_PROJET;
 
 alter table CANDIDATURE 
-   drop foreign key FK_CANDIDAT_PROPOSER__ENTREPRI;
+   drop foreign key FK_CANDIDAT_LIER_STAG_STAGE;
 
-alter table CANDIDATURE 
-   drop foreign key FK_CANDIDAT_PROPOSER__PROFESSE;
+alter table ENSEIGNER 
+   drop foreign key FK_ENSEIGNE_ENSEIGNER_MODULE;
 
-alter table ENTREPRISE 
-   drop foreign key FK_ENTREPRI_AVOIR_ADR_ADRESSE;
-
-alter table ENTREPRISE 
-   drop foreign key FK_ENTREPRI_EMPLOYER_RESPONSA;
+alter table ENSEIGNER 
+   drop foreign key FK_ENSEIGNE_ENSEIGNER_PROFESSE;
 
 alter table ETUDIANT 
    drop foreign key FK_ETUDIANT_H_TILISAT_UTILISAT;
@@ -44,17 +40,14 @@ alter table INSCRIRE
 alter table INSCRIRE 
    drop foreign key FK_INSCRIRE_INSCRIRE2_ETUDIANT;
 
-alter table MODULE 
-   drop foreign key FK_MODULE_ENSEIGNER_PROFESSE;
-
 alter table PROFESSEUR 
    drop foreign key FK_PROFESSE_H_TILISAT_UTILISAT;
 
 alter table PROJET 
-   drop foreign key FK_PROJET_H_MODULE_MODULE;
+   drop foreign key FK_PROJET_PROPOSER__PROFESSE;
 
 alter table RESPONSABLE 
-   drop foreign key FK_RESPONSA_EMPLOYER2_ENTREPRI;
+   drop foreign key FK_RESPONSA_EMPLOYER_ENTREPRI;
 
 alter table RESPONSABLE 
    drop foreign key FK_RESPONSA_ENCADRER2_STAGE;
@@ -66,10 +59,14 @@ alter table STAGE
    drop foreign key FK_STAGE_ENCADRER_RESPONSA;
 
 alter table STAGE 
-   drop foreign key FK_STAGE_H_MODULE2_MODULE;
+   drop foreign key FK_STAGE_PROPOSER__ENTREPRI;
 
 alter table UTILISATEUR 
    drop foreign key FK_UTILISAT_AVOIR_ADR_ADRESSE;
+
+
+alter table ADRESSE 
+   drop foreign key FK_ADRESSE_AVOIR_ADR_ENTREPRI;
 
 drop table if exists ADRESSE;
 
@@ -84,34 +81,33 @@ drop table if exists CANDIDATER;
 
 
 alter table CANDIDATURE 
-   drop foreign key FK_CANDIDAT_PROPOSER__ENTREPRI;
+   drop foreign key FK_CANDIDAT_LIER_STAG_STAGE;
 
 alter table CANDIDATURE 
-   drop foreign key FK_CANDIDAT_POSSEDER_TYPE_CAN;
-
-alter table CANDIDATURE 
-   drop foreign key FK_CANDIDAT_PROPOSER__PROFESSE;
+   drop foreign key FK_CANDIDAT_LIER_PROJ_PROJET;
 
 drop table if exists CANDIDATURE;
 
 
-alter table ENTREPRISE 
-   drop foreign key FK_ENTREPRI_EMPLOYER_RESPONSA;
+alter table ENSEIGNER 
+   drop foreign key FK_ENSEIGNE_ENSEIGNER_MODULE;
 
-alter table ENTREPRISE 
-   drop foreign key FK_ENTREPRI_AVOIR_ADR_ADRESSE;
+alter table ENSEIGNER 
+   drop foreign key FK_ENSEIGNE_ENSEIGNER_PROFESSE;
+
+drop table if exists ENSEIGNER;
 
 drop table if exists ENTREPRISE;
 
-
-alter table ETUDIANT 
-   drop foreign key FK_ETUDIANT_H_TILISAT_UTILISAT;
 
 alter table ETUDIANT 
    drop foreign key FK_ETUDIANT_REALISER__PROJET;
 
 alter table ETUDIANT 
    drop foreign key FK_ETUDIANT_REALISER__STAGE;
+
+alter table ETUDIANT 
+   drop foreign key FK_ETUDIANT_H_TILISAT_UTILISAT;
 
 drop table if exists ETUDIANT;
 
@@ -124,10 +120,6 @@ alter table INSCRIRE
 
 drop table if exists INSCRIRE;
 
-
-alter table MODULE 
-   drop foreign key FK_MODULE_ENSEIGNER_PROFESSE;
-
 drop table if exists MODULE;
 
 
@@ -138,13 +130,13 @@ drop table if exists PROFESSEUR;
 
 
 alter table PROJET 
-   drop foreign key FK_PROJET_H_MODULE_MODULE;
+   drop foreign key FK_PROJET_PROPOSER__PROFESSE;
 
 drop table if exists PROJET;
 
 
 alter table RESPONSABLE 
-   drop foreign key FK_RESPONSA_EMPLOYER2_ENTREPRI;
+   drop foreign key FK_RESPONSA_EMPLOYER_ENTREPRI;
 
 alter table RESPONSABLE 
    drop foreign key FK_RESPONSA_ENCADRER2_STAGE;
@@ -159,11 +151,9 @@ alter table STAGE
    drop foreign key FK_STAGE_ENCADRER_RESPONSA;
 
 alter table STAGE 
-   drop foreign key FK_STAGE_H_MODULE2_MODULE;
+   drop foreign key FK_STAGE_PROPOSER__ENTREPRI;
 
 drop table if exists STAGE;
-
-drop table if exists TYPE_CANDIDATURE;
 
 
 alter table UTILISATEUR 
@@ -177,6 +167,7 @@ drop table if exists UTILISATEUR;
 create table ADRESSE
 (
    ID_ADRESSE           int(10) not null auto_increment  comment '',
+   ID_ENTREPRISE        int(10)  comment '',
    RUE                  varchar(100)  comment '',
    CODE_POSTAL          varchar(5)  comment '',
    VILLE                varchar(25)  comment '',
@@ -193,7 +184,8 @@ create table CANDIDATER
    ID_CANDIDATURE       int(10) not null  comment '',
    DATE_CAND            date  comment '',
    primary key (ID_PERS, ID_CANDIDATURE)
-);
+)
+auto_increment = 1;
 
 /*==============================================================*/
 /* Table : CANDIDATURE                                          */
@@ -201,12 +193,22 @@ create table CANDIDATER
 create table CANDIDATURE
 (
    ID_CANDIDATURE       int(10) not null auto_increment  comment '',
-   ID_ENTREPRISE        int(10)  comment '',
-   ID_TYPE_CANDIDATURE  int(10)  comment '',
-   ID_PERS              int(10)  comment '',
+   ID_STAGE             int(5)  comment '',
+   ID_PROJET            int(5)  comment '',
    DESCRIPTION          varchar(500)  comment '',
    ACCEPTEE             bool  comment '',
    primary key (ID_CANDIDATURE)
+)
+auto_increment = 1;
+
+/*==============================================================*/
+/* Table : ENSEIGNER                                            */
+/*==============================================================*/
+create table ENSEIGNER
+(
+   ID_MODULE            varchar(5) not null  comment '',
+   ID_PERS              int(10) not null  comment '',
+   primary key (ID_MODULE, ID_PERS)
 )
 auto_increment = 1;
 
@@ -216,8 +218,6 @@ auto_increment = 1;
 create table ENTREPRISE
 (
    ID_ENTREPRISE        int(10) not null auto_increment  comment '',
-   ID_ADRESSE           int(10)  comment '',
-   ID_PERS              int(10)  comment '',
    LIB_ENTREPRISE       varchar(100)  comment '',
    primary key (ID_ENTREPRISE)
 )
@@ -229,8 +229,8 @@ auto_increment = 1;
 create table ETUDIANT
 (
    ID_PERS              int(10) not null  comment '',
-   ID_MODULE            varchar(5)  comment '',
-   PRO_ID_MODULE        varchar(5)  comment '',
+   ID_PROJET            int(5)  comment '',
+   ID_STAGE             int(5)  comment '',
    N_ETUD               int not null  comment '',
    primary key (ID_PERS)
 )
@@ -246,7 +246,8 @@ create table INSCRIRE
    NOTE_CC              decimal(4)  comment '',
    NOTE_TP              decimal(4)  comment '',
    primary key (ID_MODULE, ID_PERS)
-);
+)
+auto_increment = 1;
 
 /*==============================================================*/
 /* Table : MODULE                                               */
@@ -254,12 +255,10 @@ create table INSCRIRE
 create table MODULE
 (
    ID_MODULE            varchar(5) not null  comment '',
-   ID_PERS              int(10)  comment '',
    LIB_MODULE           varchar(100)  comment '',
-   DATE_DEBUT           date  comment '',
-   DATE_FIN             date  comment '',
    primary key (ID_MODULE)
-);
+)
+auto_increment = 1;
 
 /*==============================================================*/
 /* Table : PROFESSEUR                                           */
@@ -277,12 +276,14 @@ auto_increment = 1;
 /*==============================================================*/
 create table PROJET
 (
-   ID_MODULE            varchar(5) not null  comment '',
-   LIB_MODULE           varchar(100)  comment '',
-   DATE_DEBUT           date  comment '',
-   DATE_FIN             date  comment '',
-   primary key (ID_MODULE)
-);
+   ID_PROJET            int(5) not null auto_increment  comment '',
+   ID_PERS              int(10)  comment '',
+   LIB_PROJET           varchar(100) not null  comment '',
+   DATE_DEB_PROJET      date  comment '',
+   DATE_FIN_PROJET      date  comment '',
+   primary key (ID_PROJET)
+)
+auto_increment = 1;
 
 /*==============================================================*/
 /* Table : RESPONSABLE                                          */
@@ -291,7 +292,7 @@ create table RESPONSABLE
 (
    ID_PERS              int(10) not null  comment '',
    ID_ENTREPRISE        int(10)  comment '',
-   ID_MODULE            varchar(5)  comment '',
+   ID_STAGE             int(5)  comment '',
    primary key (ID_PERS)
 )
 auto_increment = 1;
@@ -301,23 +302,15 @@ auto_increment = 1;
 /*==============================================================*/
 create table STAGE
 (
-   ID_MODULE            varchar(5) not null  comment '',
+   ID_STAGE             int(5) not null auto_increment  comment '',
+   ID_ENTREPRISE        int(10)  comment '',
    ID_PERS              int(10)  comment '',
-   LIB_MODULE           varchar(100)  comment '',
-   DATE_DEBUT           date  comment '',
-   DATE_FIN             date  comment '',
-   primary key (ID_MODULE)
-);
-
-/*==============================================================*/
-/* Table : TYPE_CANDIDATURE                                     */
-/*==============================================================*/
-create table TYPE_CANDIDATURE
-(
-   ID_TYPE_CANDIDATURE  int(10) not null auto_increment  comment '',
-   LIB_TYPE_CANDIDATURE int not null  comment '',
-   primary key (ID_TYPE_CANDIDATURE)
-);
+   LIB_STAGE            varchar(100)  comment '',
+   DATE_DEB_STAGE       date not null  comment '',
+   DATE_FIN_STAGE       date not null  comment '',
+   primary key (ID_STAGE)
+)
+auto_increment = 1;
 
 /*==============================================================*/
 /* Table : UTILISATEUR                                          */
@@ -332,10 +325,13 @@ create table UTILISATEUR
    DATENAIS             date  comment '',
    MAIL                 varchar(100)  comment '',
    LOGIN                varchar(25) not null  comment '',
-   SHA_MDP              varchar(25) not null  comment '',
+   MDP                  varchar(25) not null  comment '',
    primary key (ID_PERS)
 )
 auto_increment = 1;
+
+alter table ADRESSE add constraint FK_ADRESSE_AVOIR_ADR_ENTREPRI foreign key (ID_ENTREPRISE)
+      references ENTREPRISE (ID_ENTREPRISE) on delete restrict on update restrict;
 
 alter table CANDIDATER add constraint FK_CANDIDAT_CANDIDATE_ETUDIANT foreign key (ID_PERS)
       references ETUDIANT (ID_PERS) on delete restrict on update restrict;
@@ -343,29 +339,26 @@ alter table CANDIDATER add constraint FK_CANDIDAT_CANDIDATE_ETUDIANT foreign key
 alter table CANDIDATER add constraint FK_CANDIDAT_CANDIDATE_CANDIDAT foreign key (ID_CANDIDATURE)
       references CANDIDATURE (ID_CANDIDATURE) on delete restrict on update restrict;
 
-alter table CANDIDATURE add constraint FK_CANDIDAT_POSSEDER_TYPE_CAN foreign key (ID_TYPE_CANDIDATURE)
-      references TYPE_CANDIDATURE (ID_TYPE_CANDIDATURE) on delete restrict on update restrict;
+alter table CANDIDATURE add constraint FK_CANDIDAT_LIER_PROJ_PROJET foreign key (ID_PROJET)
+      references PROJET (ID_PROJET) on delete restrict on update restrict;
 
-alter table CANDIDATURE add constraint FK_CANDIDAT_PROPOSER__ENTREPRI foreign key (ID_ENTREPRISE)
-      references ENTREPRISE (ID_ENTREPRISE) on delete restrict on update restrict;
+alter table CANDIDATURE add constraint FK_CANDIDAT_LIER_STAG_STAGE foreign key (ID_STAGE)
+      references STAGE (ID_STAGE) on delete restrict on update restrict;
 
-alter table CANDIDATURE add constraint FK_CANDIDAT_PROPOSER__PROFESSE foreign key (ID_PERS)
+alter table ENSEIGNER add constraint FK_ENSEIGNE_ENSEIGNER_MODULE foreign key (ID_MODULE)
+      references MODULE (ID_MODULE) on delete restrict on update restrict;
+
+alter table ENSEIGNER add constraint FK_ENSEIGNE_ENSEIGNER_PROFESSE foreign key (ID_PERS)
       references PROFESSEUR (ID_PERS) on delete restrict on update restrict;
-
-alter table ENTREPRISE add constraint FK_ENTREPRI_AVOIR_ADR_ADRESSE foreign key (ID_ADRESSE)
-      references ADRESSE (ID_ADRESSE) on delete restrict on update restrict;
-
-alter table ENTREPRISE add constraint FK_ENTREPRI_EMPLOYER_RESPONSA foreign key (ID_PERS)
-      references RESPONSABLE (ID_PERS) on delete restrict on update restrict;
 
 alter table ETUDIANT add constraint FK_ETUDIANT_H_TILISAT_UTILISAT foreign key (ID_PERS)
       references UTILISATEUR (ID_PERS) on delete restrict on update restrict;
 
-alter table ETUDIANT add constraint FK_ETUDIANT_REALISER__PROJET foreign key (PRO_ID_MODULE)
-      references PROJET (ID_MODULE) on delete restrict on update restrict;
+alter table ETUDIANT add constraint FK_ETUDIANT_REALISER__PROJET foreign key (ID_PROJET)
+      references PROJET (ID_PROJET) on delete restrict on update restrict;
 
-alter table ETUDIANT add constraint FK_ETUDIANT_REALISER__STAGE foreign key (ID_MODULE)
-      references STAGE (ID_MODULE) on delete restrict on update restrict;
+alter table ETUDIANT add constraint FK_ETUDIANT_REALISER__STAGE foreign key (ID_STAGE)
+      references STAGE (ID_STAGE) on delete restrict on update restrict;
 
 alter table INSCRIRE add constraint FK_INSCRIRE_INSCRIRE_MODULE foreign key (ID_MODULE)
       references MODULE (ID_MODULE) on delete restrict on update restrict;
@@ -373,20 +366,17 @@ alter table INSCRIRE add constraint FK_INSCRIRE_INSCRIRE_MODULE foreign key (ID_
 alter table INSCRIRE add constraint FK_INSCRIRE_INSCRIRE2_ETUDIANT foreign key (ID_PERS)
       references ETUDIANT (ID_PERS) on delete restrict on update restrict;
 
-alter table MODULE add constraint FK_MODULE_ENSEIGNER_PROFESSE foreign key (ID_PERS)
-      references PROFESSEUR (ID_PERS) on delete restrict on update restrict;
-
 alter table PROFESSEUR add constraint FK_PROFESSE_H_TILISAT_UTILISAT foreign key (ID_PERS)
       references UTILISATEUR (ID_PERS) on delete restrict on update restrict;
 
-alter table PROJET add constraint FK_PROJET_H_MODULE_MODULE foreign key (ID_MODULE)
-      references MODULE (ID_MODULE) on delete restrict on update restrict;
+alter table PROJET add constraint FK_PROJET_PROPOSER__PROFESSE foreign key (ID_PERS)
+      references PROFESSEUR (ID_PERS) on delete restrict on update restrict;
 
-alter table RESPONSABLE add constraint FK_RESPONSA_EMPLOYER2_ENTREPRI foreign key (ID_ENTREPRISE)
+alter table RESPONSABLE add constraint FK_RESPONSA_EMPLOYER_ENTREPRI foreign key (ID_ENTREPRISE)
       references ENTREPRISE (ID_ENTREPRISE) on delete restrict on update restrict;
 
-alter table RESPONSABLE add constraint FK_RESPONSA_ENCADRER2_STAGE foreign key (ID_MODULE)
-      references STAGE (ID_MODULE) on delete restrict on update restrict;
+alter table RESPONSABLE add constraint FK_RESPONSA_ENCADRER2_STAGE foreign key (ID_STAGE)
+      references STAGE (ID_STAGE) on delete restrict on update restrict;
 
 alter table RESPONSABLE add constraint FK_RESPONSA_H_TILISAT_UTILISAT foreign key (ID_PERS)
       references UTILISATEUR (ID_PERS) on delete restrict on update restrict;
@@ -394,23 +384,9 @@ alter table RESPONSABLE add constraint FK_RESPONSA_H_TILISAT_UTILISAT foreign ke
 alter table STAGE add constraint FK_STAGE_ENCADRER_RESPONSA foreign key (ID_PERS)
       references RESPONSABLE (ID_PERS) on delete restrict on update restrict;
 
-alter table STAGE add constraint FK_STAGE_H_MODULE2_MODULE foreign key (ID_MODULE)
-      references MODULE (ID_MODULE) on delete restrict on update restrict;
+alter table STAGE add constraint FK_STAGE_PROPOSER__ENTREPRI foreign key (ID_ENTREPRISE)
+      references ENTREPRISE (ID_ENTREPRISE) on delete restrict on update restrict;
 
 alter table UTILISATEUR add constraint FK_UTILISAT_AVOIR_ADR_ADRESSE foreign key (ID_ADRESSE)
       references ADRESSE (ID_ADRESSE) on delete restrict on update restrict;
-
-
-create or replace trigger INSERT_PASS_TO_SHA before insert
-on UTILISATEUR for each row
-begin
-    set new.SHA_MDP = sha2(new.SHA_MDP,512);
-end;
-
-
-create or replace trigger UPDATE_PASS_TO_SHA before update
-on UTILISATEUR for each row
-begin
-    set new.SHA_MDP = sha2(new.SHA_MDP,512);
-end;
 
