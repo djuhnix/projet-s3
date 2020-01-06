b88decb3251aaa9b1c75ed9a8878d185763853a0
Zermani Rayan <rayan.zermani@etudiant.univ-reims.fr>
2020-01-06 16:31:31 +0100

    update note

--- prod/php/class/Notes.class.php
+++ prod/php/class/Notes.class.php
@@ -1,144 +1,173 @@
 <?php
 
-require_once('autoload.include.php') ;
+require_once('autoload.inc.php') ;
 //require_once('MyPDO.template.php') ;
 //require_once('MyPDO.class.php') ;
 
-class Notes extends Entity
+class Note extends Entity
 {
-    private $id_module ;
-    private $id;
-    private $noteCc;
-    private $noteTP;
-    //note table inscrire(idmodule , nperso note Cc note cc)
+	/**
+	 * @var string $idModule
+	 */
+	private $idModule;
+	/**
+	 * @var Module $module
+	 *
+	 */
+	private $module;
+	/**
+	 * @var int $idEtudiant
+	 */
+	private $idEtudiant;
+	/**
+	 * @var Etudiant $etudiant
+	 */
+	private $etudiant;
+	/**
+	 * @var float $noteCC
+	 */
+	private $noteCC;
+	/**
+	 * @var float $noteTP
+	 */
+	private $noteTP;
 
-    public function __construct( string $id_module,string $etudiant_id_pers,float $noteCc, float $noteTP )
-    {
-      $this->id_module=$id_module
-      $this->etudiant_id_pers=$etudiant_id_pers;
-      $this->noteCc=$noteCc;
-      $this->noteTP=$noteTP;
-    }
-    public function createfromid(int $id)
-    {
-        $stmtModule = MyPDO::getInstance()->prepare(<<<SQL
-            SELECT  ID_MODULE as _id,
-                    etudiant_id_pers as etudiant_id_pers,
-                    noteCc as noteCc,
-                    noteTP as noteTP,
-            FROM Note n, inscrire i, Module m
-            WHERE   n.id_note=i.id_note
-                    and i.id_module=m.id_module
-                    and ID_MODULE = ?
-                    and ID_Pers = ?
+	/**
+	 * @param int $id
+	 * @return Note
+	 * @throws Exception
+	 */
+	public static function createFromId(int $idModule) : Note
+	{
+		$stmt = MyPDO::getInstance()->prepare(<<<SQL
+            SELECT  ID_MODULE as idModule,
+                    etudiant_id_pers as idEtudiant,
+                    note_cc as noteCC,
+                    note_tp as noteTP
+            FROM inscrire i
+            WHERE 
 SQL
-        );
-        // execute module query
-        $stmtModule->setFetchMode(PDO::FETCH_CLASS, Note::class);
-        $stmtModule->execute((''$id);
+		);
+		// execute module query
+		$stmt->setFetchMode(PDO::FETCH_CLASS, Note::class);
+		$stmt->execute([$id]);
 
-        return $stmtModule->fetchAll();
-    }
+		return $stmt->fetchAll();
+	}
 
-
-    public function getFromModuleId(int $id)
-    {
-        $stmt = MyPDO::getInstance()->prepare(<<<SQL
-            SELECT *
-            FROM Module m JOIN inscrire i on m.id_module = i.id_module
+	/**
+	 * @param int $id
+	 * @return Note[]
+	 * @throws Exception
+	 */
+	public function getFromModuleId(int $id) : array
+	{
+		$stmt = MyPDO::getInstance()->prepare(<<<SQL
+            SELECT i.id_module as idModule,
+                    etudiant_id_pers as idEtudiant,
+                    note_cc as noteCC,
+                    note_tp as noteTP
+            FROM inscrire i  JOIN Module m on m.id_module = i.id_module
             WHERE id_module = ?
 SQL
-        );
-        $stmt->setFetchMode(PDO::FETCH_CLASS, Module::class);
-        $stmt->execute((":$id"=>$id));
-        $object = $stmt->fetchAll();
-        if (false === $object) {
-            throw new Exception('Aucunne Proposition de stage avec l\'identifiant de stage : '.$id);
-        }
+		);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, Note::class);
+		$stmt->execute([$id]);
 
-        return $object;
-    }
+		return  $stmt->fetchAll();
+	}
 
-    public function getFromEtudiantId(int $id)
-    {
-        $stmt = MyPDO::getInstance()->prepare(<<<SQL
-            SELECT *
-            FROM Etudiant e JOIN inscrire i on e.id_pers = i.id_pers
-            WHERE id_Etudiant_pers = ?
+	/**
+	 * @param int $id
+	 * @return Etudiant[]
+	 * @throws Exception
+	 */
+	public function getFromEtudiantId(int $id)
+	{
+		$stmt = MyPDO::getInstance()->prepare(<<<SQL
+            SELECT id_module as idModule,
+                    etudiant_id_pers as idEtudiant,
+                    note_cc as noteCC,
+                    note_tp as noteTP
+            FROM inscrire i JOIN etudiant e on e.id_pers = i.etudiant_id_pers
+            WHERE etudiant_id_pers= ?
 SQL
-        );
-        $stmt->setFetchMode(PDO::FETCH_CLASS, Note::class);
-        $stmt->execute((":$id"=>$id));
-        $object = $stmt->fetchAll();
-        if (false === $object) {
-            throw new Exception('Aucunne Proposition de stage avec l\'identifiant de stage : '.$id);
-        }
+		);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, Note::class);
+		$stmt->execute(([$id]));
 
-        return $object;
-    }
+		return  $stmt->fetchAll();
+	}
 
-    public function getTitleModule():string
-    {
-      return this->$libele;
-    }
+	public function getTitleModule():string
+	{
+		return $this->$libele;
+	}
 
-    public function getNoteCc():float
-    {
-        return this->noteCc;
-    }
-    public function getNoteTP():float
-    {
-        return this->noteTP;
-    }
-    public function setNoteCc($nouvelNote):void
-    {
-         this->noteCc=$nouvellNote;
-    }
-    public function setNoteTP($nouvelNote):void
-    {
-         this->noteTP=$nouvelNote;
-    }
-    public function getMoyenne():float
-    {
-        $val=this->noteCc;
-        $val2=this>noteTP;
-        $moyenne=($val+$val2)/2;
-        return $moyenne;
-    }
+	public function getNoteCc():float
+	{
+		return $this->noteCc;
+	}
+	public function getNoteTP():float
+	{
+		return $this->noteTP;
+	}
+	public function setNoteCc($nouvelNote):void
+	{
+		$this->noteCc=$nouvellNote;
+	}
+	public function setNoteTP($nouvelNote):void
+	{
+		$this->noteTP=$nouvelNote;
+	}
+	public function getMoyenne():float
+	{
+		$val=$this->noteCC;
+		$val2=$this>noteTP;
+		$moyenne=($val+$val2)/2;
+		return $moyenne;
+	}
 
-    public static function ajouterNote(): bool
-    {
-    $stmt = MyPDO::getInstance()->prepare(<<<SQL
-        INSERT INTO Note (`id_module`, `etudiant_id_pers`, `noteCc`, `noteTP`)
+	public function persist(): bool
+	{
+		$stmt = MyPDO::getInstance()->prepare(<<<SQL
+        INSERT INTO inscrire (id_module, etudiant_id_pers, note_cc, note_tp)
         VALUES (
                 :id_module,
                 :etudiant_id_pers,
-                :noteCc,
+                :noteCC,
                 :noteTP
             );
 SQL
-    );
-    return $stmt->execute([
-        ':id_module' => $this->_id,
-        ':etudiant_id_pers' => $this->etudiant_id_pers,
-        ':noteCc' => $this->noteCc,
-        ':noteTP' => $this->noteTP,
-        ]);
-    }
-    public function getAll($id_module, id_pers, $note):string
-    {
-            $stmt = MyPDO::getInstance()->prepare(<<<SQL
-            SELECT *
-            FROM Note
+		);
+		return $stmt->execute([
+			':id_module' => $this->idModule,
+			':etudiant_id_pers' => $this->idEtudiant,
+			':noteCC' => $this->noteCC,
+			':noteTP' => $this->noteTP,
+		]);
+	}
+
+	/**
+	 * @return Note[]
+	 * @throws Exception
+	 */
+	public static function getAll() : array
+	{
+		$stmt = MyPDO::getInstance()->prepare(<<<SQL
+            SELECT id_module as idModule,
+                    etudiant_id_pers as idEtudiant,
+                    note_cc as noteCC,
+                    note_tp as noteTP
+            FROM inscrire
 SQL
-    );
-    $stmt->setFetchMode(PDO::FETCH_CLASS, Note::class);
-    $stmt->execute();
+		);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, Note::class);
+		$stmt->execute();
 
-    return $stmt->fetchAll();
-}
+		return $stmt->fetchAll();
+	}
 
 
-    }
 
 }
